26835b1fce2a14960bb26d0ae840d218e46d7df4
Vegard Kjeka Broen <vegarbro@ulrik.uio.no>
2024-06-12 16:32:00 +0200

    Transition to init.lua   - change colorscheme to rose-pine

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,8 @@
 vim.wo.number = true
 vim.wo.relativenumber = true
 
+vim.cmd([["vnoremap <C-r> "hy:%s/<C-r>h//gc<left><left><left>]])
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -16,5 +18,6 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   {"rose-pine/neovim", name = "rose-pine"}	
-
 })
+
+vim.cmd("colorscheme rose-pine")
--- .config/nvim/init.vim
+++
@@ -1,8 +0,0 @@
-vnoremap <C-r> "hy:%s/<C-r>h//gc<left><left><left>
-
-
-call plug#begin()
-
-
-call plug#end()
-
